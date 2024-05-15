af30a3a7823dc3eaaee00cd518b55387ba7765b9
jafar <se.hasanjafar@gmail.com>
2024-05-15 11:42:47 +0530

    commit4

--- src/test/java/com/driver/test/TestCases.java
+++ src/test/java/com/driver/test/TestCases.java
@@ -10,9 +10,24 @@ import org.mockito.InjectMocks;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @SpringBootTest(classes = Application.class)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class TestCases {
+//    @Test
+//    public void testMessageConstructor() {
+//        int id = 1;
+//        String content = "Hello, World!";
+//        Date date = new Date();
+//
+//        // Create an instance of Message with the new constructor
+//        Message message = new Message(id, content, date);
+//
+//        // Verify the fields
+//        assertEquals(id, message.getId());
+//        assertEquals(content, message.getContent());
+//        assertEquals(date, message.getTimestamp());
+//    }
 }
